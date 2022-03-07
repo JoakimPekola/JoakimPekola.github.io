23b97edca15acddead0411262e6f48e176b12b3c
JoakimPekola <joakim.pekola@edu.keuda.fi>
2022-03-07 14:21:00 +0200

    Update kirjautuminen.js

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", onkokirjautunut);
-
+let a = 1
 function onkokirjautunut(){
     
     if(localStorage.getItem("kirjautunut") ==='kylla'){
         document.getElementById("tevetuloa_teksti").textContent += " " + localStorage.getItem("nimi");
-        document.getElementById("kirjautumis_lomake").style.display = "none";
-        document.getElementById("kirjaudu_ulos").style.display ="none";
+        a = 2
+        
     }
 }
-
+function kirjaudulos(a){
+    if(a===1){
+        document.getElementById("kirjaudu_ulos").style.display="none";
+    }else{
+            document.getElementById("kirjaudu_ulos").style.display="block";
+        }
+    
+}
 function kirjaudu(){
     localStorage.setItem("nimi",  document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut","kylla")
