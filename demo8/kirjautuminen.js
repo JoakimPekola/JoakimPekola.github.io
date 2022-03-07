cf8f22745aaa4fcf4a2f1be532e5ad70e9ef1e95
JoakimPekola <joakim.pekola@edu.keuda.fi>
2022-03-07 13:49:10 +0200

    a

---
+++ demo8/kirjautuminen.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", onkokirjautunut);
+function onkokirjautunut(){
+    let kirjautunut = 'kylla';
+    if(kirjautunut ==='kylla'){
+        document.getElementById("tevetuloa_teksti").textContent += " " + localStorage.getItem("nimi");
+    }
+}
+
+function kirjaudu(){
+    localStorage.setItem("nimi",  document.getElementById("nimi").value);
+}
